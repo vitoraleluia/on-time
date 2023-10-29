0ac1e855f0551bd6fc2675c7adae0cec74916fb2
Vitor Aleluia <vitoraleluia@outlook.pt>
2023-10-29 15:59:43 +0000

    first commit

---
+++ src/test/java/com/ontime/ontime/OntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontime.ontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
